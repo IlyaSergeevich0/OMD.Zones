637195f1b2eed718af513ec9f691dcd43a8eac1b
IlyaSergeevich0 <defaultdev0x101@gmail.com>
2025-10-14 14:54:03 +0300

    feat: remade events to support new zones

--- OMD.Zones/Events/Listeners/LevelEventListeners.cs
+++ OMD.Zones/Events/Listeners/LevelEventListeners.cs
@@ -1,8 +1,6 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
-using OMD.Zones.Main;
 using OMD.Zones.Services.API;
 using OpenMod.API.Eventing;
-using OpenMod.API.Plugins;
 using OpenMod.Core.Eventing;
 using OpenMod.Unturned.Level.Events;
 using System.Threading.Tasks;
@@ -10,14 +8,12 @@ using System.Threading.Tasks;
 namespace OMD.Zones.Events.Listeners;
 
 [EventListenerLifetime(ServiceLifetime.Transient)]
-public sealed class LevelEventListeners(IZonesService zonesService, IPluginAccessor<ZonesPlugin> pluginAccessor) :
+public sealed class LevelEventListeners(IZonesRegistry zonesRegistry) :
     IEventListener<UnturnedPostLevelLoadedEvent>
 {
-    [EventListener(IgnoreCancelled = true, Priority = EventListenerPriority.Highest)]
+    [EventListener(IgnoreCancelled = true, Priority = EventListenerPriority.Monitor)]
     public async Task HandleEventAsync(object? sender, UnturnedPostLevelLoadedEvent @event)
     {
-        var plugin = pluginAccessor.Instance!;
-
-        await zonesService.Initialize(plugin);
+        await zonesRegistry.InitializeAsync();
     }
 }
--- OMD.Zones/Events/Models/ZonesServiceInitializedEvent.cs
+++
@@ -1,5 +0,0 @@
-﻿using OpenMod.Core.Eventing;
-
-namespace OMD.Zones.Events.Models;
-
-public sealed class ZonesServiceInitializedEvent : Event { }
